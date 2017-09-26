fa12d2efb33fbe99d2e27fa1bef91b4cbd5ef5ac
Tim Shi <stl501@gmail.com>
2017-09-26 16:11:56 -0700

    Test conda2 env

--- serve.py
+++ serve.py
@@ -6,7 +6,7 @@ model = json.load(open('model.json', 'r'))
 
 def predict(x1, x2):
     if model['w1'] * x1 + model['w2'] * x2 > 0:
-        return {'answer': 1.}
+        return {'out': 1.}
     else:
-        return {'answer': 0.}
+        return {'out': 0.}
 
