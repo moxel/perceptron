3e1f46b2d2f74fda61aacbdf9c7b75bda074c44c
Tim Shi <stl501@gmail.com>
2017-09-27 12:31:20 -0700

    Add login flow to test push

--- test_push.py
+++ test_push.py
@@ -5,6 +5,12 @@ import sys
 env = os.environ
 env.update({'ENV': 'dev'})
 
+child = pexpect.spawn('moxel login -c', env=env, logfile=sys.stdout.buffer)
+child.expect('User')
+child.send('bot\n')
+child.expect('Password')
+child.send('scarlett-johansson\n')
+
 child = pexpect.spawn('moxel push -y ci-perceptron:latest', env=env, logfile=sys.stdout.buffer)
 try:
     child.expect('Running on http://0.0.0.0:5900/', timeout=360)
